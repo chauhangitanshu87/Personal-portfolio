daa0e5553b090a9b0b4b15c983f1426158994e0e
girishrana <ranagirish246@gmail.com>
2024-09-20 08:20:29 +0530

    final

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
             <div className="card-body">
               <h3 className="card-title">Covid-19 Disease Prediction</h3>
               <p className="card-text">Used Linear Regression and KNN algorithms to predict upcoming COVID-19 cases and classify diseases.</p>
-              <a href="https://github.com/chauhangitanshu87/covid19-prediction" target="_blank" rel="noopener noreferrer" className="btn btn-dark">View on GitHub</a>
+              <a href="https://github.com/chauhangitanshu87/Covid-Prediction-Using-ML-" target="_blank" rel="noopener noreferrer" className="btn btn-dark">View on GitHub</a>
             </div>
           </div>
         </div>
@@ -20,10 +20,34 @@ const Projects = () => {
             <div className="card-body">
               <h3 className="card-title">React News Portal</h3>
               <p className="card-text">A web application that fetches real-time news articles using the News API service and displays them in a user-friendly way.</p>
-              <a href="https://github.com/chauhangitanshu87/react-news-portal" target="_blank" rel="noopener noreferrer" className="btn btn-dark">View on GitHub</a>
+              <a href="https://github.com/chauhangitanshu87/News-Portal" target="_blank" rel="noopener noreferrer" className="btn btn-dark">View on GitHub</a>
             </div>
           </div>
         </div>
+
+        <div className="col-md-6">
+          <div className="card mb-4">
+            <div className="card-body">
+              <h3 className="card-title">Product Reviews Sentimental Analysis Using Machine Learning.</h3>
+              <p className="card-text">In this project, I have classified product reviews from E-commerce website into positive,
+                negative, and neutral category, with the help of machine learning and Natural language
+                Processing.</p>
+              <a href="https://github.com/chauhangitanshu87/Product-Reviews-Sentimental-Analysis" target="_blank" rel="noopener noreferrer" className="btn btn-dark">View on GitHub</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className="card mb-4">
+            <div className="card-body">
+              <h3 className="card-title">
+                Text-Utilities Website</h3>
+              <p className="card-text">The objective of a Text Utils website is to provide users with a variety of tools and utilities to manipulate, analyze, and enhance text.</p>
+              <a href="https://github.com/chauhangitanshu87/Text-Utilities" target="_blank" rel="noopener noreferrer" className="btn btn-dark">View on GitHub</a>
+            </div>
+          </div>
+        </div>
+
       </div>
     </section>
   );
